d95bc65279d9af29620522acfd15c92bb4106305
Amit Dugar <amit@deforay.com>
2013-10-02 15:13:34 +0530

    Changed EID Assay Table names to begin with "r_"

--- application/models/DbTable/EidDetectionAssay.php
+++ application/models/DbTable/EidDetectionAssay.php
@@ -3,7 +3,7 @@
 class Application_Model_DbTable_EidDetectionAssay extends Zend_Db_Table_Abstract
 {
 
-    protected $_name = 'eid_detection_assay';
+    protected $_name = 'r_eid_detection_assay';
     protected $_primary = 'id';
 
 }
--- application/models/DbTable/EidExtractionAssay.php
+++ application/models/DbTable/EidExtractionAssay.php
@@ -3,7 +3,7 @@
 class Application_Model_DbTable_EidExtractionAssay extends Zend_Db_Table_Abstract
 {
 
-    protected $_name = 'eid_extraction_assay';
+    protected $_name = 'r_eid_extraction_assay';
     protected $_primary = 'id';
 
 
