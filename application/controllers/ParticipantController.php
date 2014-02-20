bc9aca6ac1bccb9f615b1bf98d57dda6433de5c2
Ilahir Rahman <ilahir@deforay.com>
2014-02-20 11:12:46 +0530

    PDF generated path modified and added the editor

--- application/controllers/ParticipantController.php
+++ application/controllers/ParticipantController.php
@@ -155,6 +155,7 @@ class ParticipantController extends Zend_Controller_Action
             $id = (int) base64_decode($this->_getParam('d92nl9d8d'));
 	    $db = Zend_Db_Table_Abstract::getDefaultAdapter();
             $this->view->result = $db->fetchRow($db->select()->from(array('spm'=>'shipment_participant_map'),array('spm.map_id'))
+				->join(array('s'=>'shipment'),'s.shipment_id=spm.shipment_id',array('s.shipment_code'))
 				->join(array('p'=>'participant'),'p.participant_id=spm.participant_id',array('p.first_name','p.last_name'))
 				->where("spm.map_id = ?",$id));
 	    
