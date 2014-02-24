8402cd7527cb090ebdd42101925baa8506c9488c
Ilahir Rahman <ilahir@deforay.com>
2014-02-24 19:26:27 +0530

    Added report config link in manage menu

--- application/modules/admin/controllers/ReportConfigController.php
+++ application/modules/admin/controllers/ReportConfigController.php
@@ -5,7 +5,7 @@ class Admin_ReportConfigController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->_helper->layout()->pageName = 'manage';
     }
 
     public function indexAction()
