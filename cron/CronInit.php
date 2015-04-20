164dd379457212374404e3001e57d95762a7d68c
Gunaseelan <guna@deforay.com>
2015-04-20 11:06:55 +0530

    mail cron part finished.

---
+++ cron/CronInit.php
@@ -0,0 +1,41 @@
+<?php
+ 
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+ 
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', 'development');
+    
+// Define CRON PATH 
+defined('CRON_PATH')
+    || define('CRON_PATH', realpath(dirname(__FILE__)));
+ 
+
+// Define path to u directory
+defined('UPLOAD_PATH')
+    || define('UPLOAD_PATH', realpath(dirname(__FILE__) . '/../public/uploads'));
+// Define path to u directory
+defined('TEMP_UPLOAD_PATH')
+    || define('TEMP_UPLOAD_PATH', realpath(dirname(__FILE__) . '/../public/temporary')); 
+ 
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+ 
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+
+Zend_Session::$_unitTestEnabled = true; 
+
+$application->bootstrap();
---
+++ cron/SendMailAlerts.php
@@ -0,0 +1,69 @@
+<?php
+
+include_once 'CronInit.php';
+
+$conf = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
+
+try {
+
+    $db = Zend_Db::factory($conf->resources->db);
+    Zend_Db_Table::setDefaultAdapter($db);
+
+    $conf = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
+    $smtpTransportObj = new Zend_Mail_Transport_Smtp($conf->email->host, $conf->email->config->toArray());
+
+    $limit = '10';
+    $sQuery = $db->select()->from(array('tm' => 'temp_mail'))->where("tm.status=?",'pending')->limit($limit);
+    $mailResult = $db->fetchAll($sQuery);
+    
+    //error_log('RUNNING CRON TO SEND MAIL PA');
+    
+    if (count($mailResult) > 0) {
+        foreach ($mailResult as $result) {
+            $alertMail = new Zend_Mail();
+            $id = "temp_id=" . $result['temp_id'];
+            $db->update('temp_mail',array('status'=>'not-sent'), 'temp_id=' . $result['temp_id']);
+                $fromEmail = $result['from_mail'];
+                $fromFullName = $result['from_full_name'];
+                $subject = $result['subject'];
+                $alertMail->setBodyHtml($result['message']);
+                $alertMail->setFrom($fromEmail, $fromFullName);
+                $alertMail->setReplyTo($fromEmail, $fromFullName);
+                
+                $toArray = explode(",",$result['to_email']);
+                foreach($toArray as $toId){
+                    if($toId!=''){
+                       $alertMail->addTo($toId); 
+                    }
+                }
+                 if (isset($result['cc']) && trim($result['cc']) != "") {
+                        $ccArray = explode(",", $result['cc']);
+                        foreach ($ccArray as $ccId) {
+                            if ($ccId != '') {
+                                $alertMail->addCc($ccId);
+                            }
+                        }
+                    }
+
+                    if (isset($result['bcc']) && trim($result['bcc']) != "") {
+                        $bccArray = explode(",", $result['bcc']);
+                        foreach ($bccArray as $bccId) {
+                            if ($bccId != '') {
+                                $alertMail->addBcc($bccId);
+                            }
+                        }
+                    }
+                
+                $alertMail->setSubject($subject);
+                $sendResult=$alertMail->send($smtpTransportObj);
+                if($sendResult==true){
+                  $db->delete('temp_mail', $id);
+                }
+               
+        }
+    }
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    error_log($e->getTraceAsString());
+    error_log('whoops! Something went wrong in cron/SendMailAlerts.php');
+}
