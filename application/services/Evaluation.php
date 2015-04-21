8b398dc0bcc60f8012a91ebd7cb22b35edf31883
Gunaseelan <guna@deforay.com>
2015-04-21 19:06:21 +0530

    some changes in shipment edit part.

--- application/services/Evaluation.php
+++ application/services/Evaluation.php
@@ -162,7 +162,7 @@ class Application_Service_Evaluation {
         $db = Zend_Db_Table_Abstract::getDefaultAdapter();
         $sql = $db->select()->from(array('s' => 'shipment'))
                 ->join(array('d' => 'distributions'), 'd.distribution_id=s.distribution_id')
-                ->join(array('sp' => 'shipment_participant_map'), 'sp.shipment_id=s.shipment_id', array('map_id', 'responseDate' => 'shipment_test_report_date', 'participant_count' => new Zend_Db_Expr('count("participant_id")'), 'reported_count' => new Zend_Db_Expr("SUM(shipment_test_date <> '0000-00-00')"), 'number_passed' => new Zend_Db_Expr("SUM(final_result = 1)"),'last_not_participated_mailed_on','last_not_participated_mail_count'))
+                ->join(array('sp' => 'shipment_participant_map'), 'sp.shipment_id=s.shipment_id', array('map_id', 'responseDate' => 'shipment_test_report_date', 'participant_count' => new Zend_Db_Expr('count("participant_id")'), 'reported_count' => new Zend_Db_Expr("SUM(shipment_test_date <> '0000-00-00')"), 'number_passed' => new Zend_Db_Expr("SUM(final_result = 1)"), 'last_not_participated_mailed_on', 'last_not_participated_mail_count'))
                 ->join(array('sl' => 'scheme_list'), 'sl.scheme_id=s.scheme_type')
                 ->joinLeft(array('rr' => 'r_results'), 'sp.final_result=rr.result_id')
                 ->where("s.distribution_id = ?", $distributionId)
@@ -1197,6 +1197,27 @@ class Application_Service_Evaluation {
         $authNameSpace = new Zend_Session_Namespace('administrators');
         $admin = $authNameSpace->primary_email;
         $size = count($params['sampleId']);
+
+        $shipdata = array(
+            "shipment_date" => Pt_Commons_General::dateFormat($params['shipmentDate']),
+            "lastdate_response" => Pt_Commons_General::dateFormat($params['responseDate']),
+            "updated_by_admin" => $admin,
+           "updated_on_admin" => new Zend_Db_Expr('now()')
+        );
+       
+        $db->update('shipment', $shipdata, "shipment_id = " . $params['shipmentId']);
+        $attributes["sample_rehydration_date"] = Pt_Commons_General::dateFormat($params['rehydrationDate']);
+        $attributes["algorithm"] = $params['algorithm'];
+        $attributes = json_encode($attributes);
+        $mapdata = array(
+            "shipment_receipt_date" => Pt_Commons_General::dateFormat($params['receivedOn']),
+            "shipment_test_date" => Pt_Commons_General::dateFormat($params['testedOn']),
+            "attributes" => $attributes,
+            "updated_by_admin" => $admin,
+            "updated_on_admin" => new Zend_Db_Expr('now()')
+        );
+        $db->update('shipment_participant_map', $mapdata, "map_id = " . $params['smid']);
+
         if ($params['scheme'] == 'eid') {
             for ($i = 0; $i < $size; $i++) {
                 $db->update('response_result_eid', array('reported_result' => $params['reported'][$i], 'updated_by' => $admin, 'updated_on' => new Zend_Db_Expr('now()')), "shipment_map_id = " . $params['smid'] . " AND sample_id = " . $params['sampleId'][$i]);
