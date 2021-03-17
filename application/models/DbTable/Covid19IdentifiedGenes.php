938e6d66b788a9713ad6325aa2de90d8956dc087
thanaseelan <thanaseelan@deforay.com>
2021-03-17 20:15:09 +0530

    Few changes done

--- application/models/DbTable/Covid19IdentifiedGenes.php
+++ application/models/DbTable/Covid19IdentifiedGenes.php
@@ -7,23 +7,22 @@ class Application_Model_DbTable_Covid19IdentifiedGenes extends Zend_Db_Table_Abs
     protected $_primary = 'gene_id';
 
 	public function saveCovid19IdentifiedGenesResults($params) {
-        // Zend_Debug::dump($params);die;
         $db = Zend_Db_Table_Abstract::getAdapter();
         if(count($params['sampleId']) > 0){
             $db->delete('covid19_identified_genes', "map_id = " . $params['smid']);
             foreach($params['sampleId'] as $sample){
                 foreach($params['geneType'][$sample] as $key=>$gene){
-                    // if(isset($params['cTValue'][$sample][$key]) && $params['cTValue'][$sample][$key] > 0 && isset($params['cTValue'][$sample][$key]) && $params['cTValue'][$sample][$key] != ""){
+                    if(isset($params['geneType'][$sample][$key]) && $params['geneType'][$sample][$key] > 0 && isset($params['cTValue'][$sample][$key]) && $params['cTValue'][$sample][$key] != ""){
                         $data = array(
                             'map_id'        => $params['smid'],
                             'shipment_id'   => $params['shipmentId'],
                             'sample_id'     => $sample,
-                            'gene_id'       => $gene,
+                            'gene_id'       => $params['geneType'][$sample][$key],
                             'ct_value'      => $params['cTValue'][$sample][$key],
                             'remarks'       => $params['remarks'][$sample][$key]
                         );
                         $this->insert($data);
-                    // }
+                    }
                 }
             }
         }
