f4fc14314b29641396a1b5c050ecbfa28a5dcf0b
Thanaseelan K <thanaseelan@deforay.com>
2021-07-13 12:04:26 +0530

    Few correction done in vl model

--- application/models/Vl.php
+++ application/models/Vl.php
@@ -232,8 +232,8 @@ class Application_Model_Vl
                 }
                 /* Manual result override changes */
                 if (isset($shipment['manual_override']) && $shipment['manual_override'] == 'yes') {
-                    $sql = $this->db->select()->from('shipment_participant_map')->where("map_id = ?", $shipment['map_id']);
-                    $shipmentOverall = $this->db->fetchRow($sql);
+                    $sql = $db->select()->from('shipment_participant_map')->where("map_id = ?", $shipment['map_id']);
+                    $shipmentOverall = $db->fetchRow($sql);
                     if (sizeof($shipmentOverall) > 0) {
                         $shipmentResult[$counter]['shipment_score'] = $shipmentOverall['shipment_score'];
                         $shipmentResult[$counter]['documentation_score'] = $shipmentOverall['documentation_score'];
@@ -243,7 +243,7 @@ class Application_Model_Vl
                         $fRes = $db->fetchCol($db->select()->from('r_results', array('result_name'))->where('result_id = ' . $shipmentOverall['final_result']));
                         $shipmentResult[$counter]['display_result'] = $fRes[0];
                         // Zend_Debug::dump($shipmentResult);die;
-                        $nofOfRowsUpdated = $this->db->update('shipment_participant_map', array('shipment_score' => $shipmentOverall['shipment_score'], 'documentation_score' => $shipmentOverall['documentation_score'], 'final_result' => $shipmentOverall['final_result']), "map_id = " . $shipment['map_id']);
+                        $nofOfRowsUpdated = $db->update('shipment_participant_map', array('shipment_score' => $shipmentOverall['shipment_score'], 'documentation_score' => $shipmentOverall['documentation_score'], 'final_result' => $shipmentOverall['final_result']), "map_id = " . $shipment['map_id']);
                     }
                 } else {
 
