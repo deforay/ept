708d8f5f1b351dc6f4fdfd63d91b5e8435859fee
Amit Dugar <amit@deforay.com>
2021-12-01 14:48:55 +0530

    Added DTS to generate-certificate.php

--- scheduled-jobs/generate-certificate.php
+++ scheduled-jobs/generate-certificate.php
@@ -13,146 +13,151 @@ $vlAssayArray = $schemesService->getVlAssay();
 
 
 try {
-    
-    $db = Zend_Db::factory($conf->resources->db);
-    Zend_Db_Table::setDefaultAdapter($db);
-    
-    $output = array();
-    
-    $query = $db->select()->from(array('s' => 'shipment'), array('s.shipment_id', 's.shipment_code', 's.scheme_type', 's.shipment_date',))
-								->where("shipment_id IN (21,22)")
-								->order("s.scheme_type");
-    
-    
+
+	$db = Zend_Db::factory($conf->resources->db);
+	Zend_Db_Table::setDefaultAdapter($db);
+
+	$output = array();
+
+	$query = $db->select()->from(array('s' => 'shipment'), array('s.shipment_id', 's.shipment_code', 's.scheme_type', 's.shipment_date',))
+		->where("shipment_id IN (3,4,5,6)")
+		->order("s.scheme_type");
+
+
 	$shipmentResult = $db->fetchAll($query);
-    $shipmentIDArray=array();
-    foreach($shipmentResult as $val){
-        $shipmentIdArray[]=$val['shipment_id'];
-        $shipmentCodeArray[$val['scheme_type']][]=$val['shipment_code'];
-        $impShipmentId=implode(",",$shipmentIdArray);
-    }
-    
-    $sQuery = $db->select()->from(array('spm' => 'shipment_participant_map'), array('spm.map_id','spm.attributes','spm.shipment_id','spm.participant_id','spm.shipment_score','spm.final_result'))
-							->join(array('s' => 'shipment'),'s.shipment_id=spm.shipment_id',array('shipment_code','scheme_type'))
-							->join(array('p' => 'participant'),'p.participant_id=spm.participant_id',array('unique_identifier','first_name','last_name','email','city','state','address','institute_name'))
-							->where("spm.shipment_test_date!='0000-00-00'")
-                            ->order("scheme_type ASC");
-                            
-    $sQuery->where('spm.shipment_id IN ('.$impShipmentId.')');
-    
-    //Zend_Debug::dump($shipmentCodeArray);die;
-    $shipmentParticipantResult = $db->fetchAll($sQuery);
+	$shipmentIDArray = array();
+	foreach ($shipmentResult as $val) {
+		$shipmentIdArray[] = $val['shipment_id'];
+		$shipmentCodeArray[$val['scheme_type']][] = $val['shipment_code'];
+		$impShipmentId = implode(",", $shipmentIdArray);
+	}
+
+	$sQuery = $db->select()->from(array('spm' => 'shipment_participant_map'), array('spm.map_id', 'spm.attributes', 'spm.shipment_id', 'spm.participant_id', 'spm.shipment_score', 'spm.final_result'))
+		->join(array('s' => 'shipment'), 's.shipment_id=spm.shipment_id', array('shipment_code', 'scheme_type'))
+		->join(array('p' => 'participant'), 'p.participant_id=spm.participant_id', array('unique_identifier', 'first_name', 'last_name', 'email', 'city', 'state', 'address', 'institute_name'))
+		->where("spm.shipment_test_date!='0000-00-00'")
+		->order("scheme_type ASC");
+
+	$sQuery->where('spm.shipment_id IN (' . $impShipmentId . ')');
+
+	//Zend_Debug::dump($shipmentCodeArray);die;
+	$shipmentParticipantResult = $db->fetchAll($sQuery);
 	//Zend_Debug::dump($shipmentParticipantResult);die;
 	$participants = array();
-    foreach($shipmentParticipantResult as $shipment){
+	foreach ($shipmentParticipantResult as $shipment) {
 
 		//$assay = $vlAssayArray[$attribs]
 		//Zend_Debug::dump($attribs);die;
-        //echo count($participants);
+		//echo count($participants);
 
-		$participants[$shipment['unique_identifier']]['labName']=$shipment['first_name']." ".$shipment['last_name'];
+		$participants[$shipment['unique_identifier']]['labName'] = $shipment['first_name'] . " " . $shipment['last_name'];
+		$participants[$shipment['unique_identifier']]['city'] = $shipment['city'];
 		//$participants[$shipment['unique_identifier']]['finalResult']=$shipment['final_result'];
-		$participants[$shipment['unique_identifier']][$shipment['scheme_type']][$shipment['shipment_code']]['score']=$shipment['shipment_score'];
-		$participants[$shipment['unique_identifier']][$shipment['scheme_type']][$shipment['shipment_code']]['result']=$shipment['final_result'];
-		$participants[$shipment['unique_identifier']]['attribs']=json_decode($shipment['attributes'], true);
+		$participants[$shipment['unique_identifier']][$shipment['scheme_type']][$shipment['shipment_code']]['score'] = $shipment['shipment_score'];
+		$participants[$shipment['unique_identifier']][$shipment['scheme_type']][$shipment['shipment_code']]['result'] = $shipment['final_result'];
+		$participants[$shipment['unique_identifier']]['attribs'] = json_decode($shipment['attributes'], true);
 		//$participants[$shipment['unique_identifier']][$shipment['shipment_code']]=$shipment['shipment_score'];
-        
-    }
-    //$this->generateAnnualReport($shipmentCodeArray,$participants,$startDate,$endDate);
-    //Zend_Debug::dump($participants);die;
-    foreach($participants as $participantUID=>$arrayVal){
-			
-			foreach($shipmentCodeArray as $schemeKey=>$scheme){
-				if(isset($arrayVal[$schemeKey])){
-					$certificate=true;
-					$participated=false;
-                
-					$query = $db->select()->from('scheme_list',array('scheme_name'))->where("scheme_id=?", $schemeKey);
-					$schemeResult = $db->fetchRow($query);
-				
-					foreach($scheme as $va){
-						if(isset($arrayVal[$schemeKey][$va]['score'])){
-							if($arrayVal[$schemeKey][$va]['result']!=1){
-								$certificate=false;
-							}
-							if(trim($arrayVal[$schemeKey][$va]['score'])!=""){
-								$participated=true;
-							}
-						}else{
-							
-							$certificate=false;
-						}
-					}
-                
-					if($certificate && $participated){
-						$attribs = $arrayVal['attribs'];
-						
-						if($schemeKey == 'eid'){
-							$doc = new DOCx("certificate-template/eid-e.docx");
-							$doc->setValue("LABNAME",$arrayVal['labName']);
-							//$doc->setValue("DATE","23 December 2018");
-							//$doc->save("certificate/2017 Certificate - ".strtoupper($schemeKey)." for Lab ".str_replace('/', '_', $participantUID).".docx");					
-							$doc->save("certificate/eid/".str_replace('/', '_', $participantUID)."-".$va.".docx");
-						}else if($schemeKey=='vl'){
-							if($attribs["vl_assay"]==6){
-								if(isset($attribs["other_assay"])){
-									$assay=$attribs["other_assay"];	
-								}else{
-									$assay="Other";
-								}
-							}else{
-								$assay = (isset($attribs["vl_assay"]) && isset($vlAssayArray[$attribs["vl_assay"]])) ? $vlAssayArray[$attribs["vl_assay"]] : " Other ";	
-							}
-							$doc = new DOCx("certificate-template/vl-e.docx");
-							$doc->setValue("LABNAME",$arrayVal['labName']);
-							$doc->setValue("ASSAYNAME",$assay);
-							//$doc->setValue("DATE","23 December 2018");
-							//$doc->save("certificate/2017 Certificate - ".strtoupper($schemeKey)." for Lab ".str_replace('/', '_', $participantUID).".docx");	
-							$doc->save("certificate/vl/".str_replace('/', '_', $participantUID)."-".$va.".docx");
-						}
-						
-					}else if($participated){
 
-						// NO NEED FOR PARTICIPATED CERTIFICATES AS ON JAN 2018
-						continue;
-
-						$attribs = $arrayVal['attribs'];
-						
-						if($schemeKey == 'eid'){
-							$doc = new DOCx("certificate-template/eid-p.docx");
-							$doc->setValue("LABNAME",$arrayVal['labName']);
-							//$doc->setValue("DATE","09 January 2018");
-							//$doc->save("certificate/2017 Certificate - ".strtoupper($schemeKey)." for Lab ".str_replace('/', '-', $participantUID).".docx");	
-							$doc->save("certificate/eid/".str_replace('/', '_', $participantUID)."-".$va.".docx");
-						}else if($schemeKey=='vl'){
-							if($attribs["vl_assay"]==6){
-								if(isset($attribs["other_assay"])){
-									$assay=$attribs["other_assay"];	
-								}else{
-									$assay="Other";
-								}
-							}else{
-								$assay = (isset($attribs["vl_assay"]) && isset($vlAssayArray[$attribs["vl_assay"]])) ? $vlAssayArray[$attribs["vl_assay"]] : " Other ";	
-							}
-							
-							$doc = new DOCx("certificate-template/vl-p.docx");
-							$doc->setValue("LABNAME",$arrayVal['labName']);
-							$doc->setValue("ASSAYNAME",$assay);
-							//$doc->setValue("DATE","09 January 2018");
-							//$doc->save("certificate/2017 Certificate - ".strtoupper($schemeKey)." for Lab ".str_replace('/', '-', $participantUID).".docx");
-							$doc->save("certificate/vl/".str_replace('/', '_', $participantUID)."-".$va.".docx");
-						}
-					}
-                
-				}
-				
-			}
-			
 	}
-    
-    
+	//$this->generateAnnualReport($shipmentCodeArray,$participants,$startDate,$endDate);
+	//Zend_Debug::dump($participants);die;
+	//Zend_Debug::dump($shipmentCodeArray);die;
+	foreach ($participants as $participantUID => $arrayVal) {
+		foreach ($shipmentCodeArray as $schemeKey => $scheme) {
+			if (isset($arrayVal[$schemeKey])) {
+				$certificate = true;
+				$participated = false;
+
+				$query = $db->select()->from('scheme_list', array('scheme_name'))->where("scheme_id=?", $schemeKey);
+				$schemeResult = $db->fetchRow($query);
+
+				foreach ($scheme as $va) {
+					if (isset($arrayVal[$schemeKey][$va]['score'])) {
+						if ($arrayVal[$schemeKey][$va]['result'] != 1) {
+							$certificate = false;
+						}
+						if (trim($arrayVal[$schemeKey][$va]['score']) != "") {
+							$participated = true;
+						}
+					} else {
+
+						$certificate = false;
+					}
+				}
+
+				if ($certificate && $participated) {
+					$attribs = $arrayVal['attribs'];
+					if ($schemeKey == 'dts') {
+						$doc = new DOCx("certificate-template/dts-e.docx");
+						$doc->setValue("LABNAME", $arrayVal['labName']);
+						$doc->setValue("CITY", $arrayVal['city']);
+						$doc->save("certificate/dts/excellence/" . str_replace('/', '_', $participantUID) . "-" . $va . ".docx");
+					} else if ($schemeKey == 'eid') {
+						$doc = new DOCx("certificate-template/eid-e.docx");
+						$doc->setValue("LABNAME", $arrayVal['labName']);
+						$doc->setValue("CITY", $arrayVal['city']);
+						//$doc->setValue("DATE","23 December 2018");
+						//$doc->save("certificate/2017 Certificate - ".strtoupper($schemeKey)." for Lab ".str_replace('/', '_', $participantUID).".docx");					
+						$doc->save("certificate/eid/excellence/" . str_replace('/', '_', $participantUID) . "-" . $va . ".docx");
+					} else if ($schemeKey == 'vl') {
+						if ($attribs["vl_assay"] == 6) {
+							if (isset($attribs["other_assay"])) {
+								$assay = $attribs["other_assay"];
+							} else {
+								$assay = "Other";
+							}
+						} else {
+							$assay = (isset($attribs["vl_assay"]) && isset($vlAssayArray[$attribs["vl_assay"]])) ? $vlAssayArray[$attribs["vl_assay"]] : " Other ";
+						}
+						$doc = new DOCx("certificate-template/vl-e.docx");
+						$doc->setValue("LABNAME", $arrayVal['labName']);
+						$doc->setValue("CITY", $arrayVal['city']);
+						$doc->setValue("ASSAYNAME", $assay);
+						//$doc->setValue("DATE","23 December 2018");
+						//$doc->save("certificate/2017 Certificate - ".strtoupper($schemeKey)." for Lab ".str_replace('/', '_', $participantUID).".docx");	
+						$doc->save("certificate/vl/excellence/" . str_replace('/', '_', $participantUID) . "-" . $va . ".docx");
+					}
+				} else if ($participated) {
+
+					$attribs = $arrayVal['attribs'];
+
+					if ($schemeKey == 'dts') {
+						$doc = new DOCx("certificate-template/dts-p.docx");
+						$doc->setValue("LABNAME", $arrayVal['labName']);
+						$doc->setValue("CITY", $arrayVal['city']);
+						$doc->save("certificate/dts/participation/" . str_replace('/', '_', $participantUID) . "-" . $va . ".docx");
+					} else if ($schemeKey == 'eid') {
+						$doc = new DOCx("certificate-template/eid-p.docx");
+						$doc->setValue("LABNAME", $arrayVal['labName']);
+						$doc->setValue("CITY", $arrayVal['city']);
+						//$doc->setValue("DATE","09 January 2018");
+						//$doc->save("certificate/2017 Certificate - ".strtoupper($schemeKey)." for Lab ".str_replace('/', '-', $participantUID).".docx");	
+						$doc->save("certificate/eid/participation/" . str_replace('/', '_', $participantUID) . "-" . $va . ".docx");
+					} else if ($schemeKey == 'vl') {
+						if ($attribs["vl_assay"] == 6) {
+							if (isset($attribs["other_assay"])) {
+								$assay = $attribs["other_assay"];
+							} else {
+								$assay = "Other";
+							}
+						} else {
+							$assay = (isset($attribs["vl_assay"]) && isset($vlAssayArray[$attribs["vl_assay"]])) ? $vlAssayArray[$attribs["vl_assay"]] : " Other ";
+						}
+
+						$doc = new DOCx("certificate-template/vl-p.docx");
+						$doc->setValue("LABNAME", $arrayVal['labName']);
+						$doc->setValue("CITY", $arrayVal['city']);
+						$doc->setValue("ASSAYNAME", $assay);
+						//$doc->setValue("DATE","09 January 2018");
+						//$doc->save("certificate/2017 Certificate - ".strtoupper($schemeKey)." for Lab ".str_replace('/', '-', $participantUID).".docx");
+						$doc->save("certificate/vl/participation/" . str_replace('/', '_', $participantUID) . "-" . $va . ".docx");
+					}
+				}
+			}
+		}
+	}
 } catch (Exception $e) {
-    error_log($e->getMessage());
-    error_log($e->getTraceAsString());
-    error_log('whoops! Something went wrong in scheduled-jobs/GenerateCertificate.php');
+	error_log($e->getMessage());
+	error_log($e->getTraceAsString());
+	error_log('whoops! Something went wrong in scheduled-jobs/GenerateCertificate.php');
 }
