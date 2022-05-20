a23e48e07af693d9af897c488bc7712ac8444595
Thanaseelan K <thanaseelan@deforay.com>
2022-05-20 19:31:42 +0530

    Few changes done

--- application/models/DbTable/Shipments.php
+++ application/models/DbTable/Shipments.php
@@ -2341,56 +2341,56 @@ class Application_Model_DbTable_Shipments extends Zend_Db_Table_Abstract
                     'sampleType'                => (isset($shipment['shipment_attributes']["sampleType"]) && $shipment['shipment_attributes']["sampleType"] != '') ? $shipment['shipment_attributes']["sampleType"] : '',
                     'screeningTest'             => (isset($shipment['shipment_attributes']["screeningTest"]) && $shipment['shipment_attributes']["screeningTest"] != '') ? $shipment['shipment_attributes']["screeningTest"] : '',
                 );
-                if ((isset($config->evaluation->dts->displaySampleConditionFields) && $config->evaluation->dts->displaySampleConditionFields == "yes")) {
-                    $section2['conditionOfPTSamples'] = (isset($shipment['attributes']["condition_pt_samples"]) && $shipment['attributes']["condition_pt_samples"] != '') ? $shipment['attributes']["condition_pt_samples"] : '';
-                    $section2['refridgerator'] = (isset($shipment['attributes']["refridgerator"]) && $shipment['attributes']["refridgerator"] != '') ? $shipment['attributes']["refridgerator"] : '';
-                    $section2['roomTemperature'] = (isset($shipment['attributes']["room_temperature"]) && $shipment['attributes']["room_temperature"] != '') ? $shipment['attributes']["room_temperature"] : '';
-                    $section2['stopWatch'] = (isset($shipment['attributes']["stop_watch"]) && $shipment['attributes']["stop_watch"] != '') ? $shipment['attributes']["stop_watch"] : '';
+                // if ((isset($config->evaluation->dts->displaySampleConditionFields) && $config->evaluation->dts->displaySampleConditionFields == "yes")) {
+                $section2['conditionOfPTSamples'] = (isset($shipment['attributes']["condition_pt_samples"]) && $shipment['attributes']["condition_pt_samples"] != '') ? $shipment['attributes']["condition_pt_samples"] : '';
+                $section2['refridgerator'] = (isset($shipment['attributes']["refridgerator"]) && $shipment['attributes']["refridgerator"] != '') ? $shipment['attributes']["refridgerator"] : '';
+                $section2['roomTemperature'] = (isset($shipment['attributes']["room_temperature"]) && $shipment['attributes']["room_temperature"] != '') ? $shipment['attributes']["room_temperature"] : '';
+                $section2['stopWatch'] = (isset($shipment['attributes']["stop_watch"]) && $shipment['attributes']["stop_watch"] != '') ? $shipment['attributes']["stop_watch"] : '';
 
-                    $section2['conditionOfPTSamplesSelect'] = array(
-                        array(
-                            'value'     =>  'good',
-                            'show'      =>  'Good',
-                            'selected'  => (isset($shipment['attributes']["condition_pt_samples"]) && $shipment['attributes']["condition_pt_samples"] == 'good') ? 'selected' : ''
-                        ),
-                        array(
-                            'value'     =>  'bad',
-                            'show'      =>  'Bad',
-                            'selected'  => (isset($shipment['attributes']["condition_pt_samples"]) && $shipment['attributes']["condition_pt_samples"] == 'bad') ? 'selected' : ''
-                        ),
-                        array(
-                            'value'     =>  'not-sure',
-                            'show'      =>  'Not Sure',
-                            'selected'  => (isset($shipment['attributes']["condition_pt_samples"]) && $shipment['attributes']["condition_pt_samples"] == 'not-sure') ? 'selected' : ''
-                        )
-                    );
+                $section2['conditionOfPTSamplesSelect'] = array(
+                    array(
+                        'value'     =>  'good',
+                        'show'      =>  'Good',
+                        'selected'  => (isset($shipment['attributes']["condition_pt_samples"]) && $shipment['attributes']["condition_pt_samples"] == 'good') ? 'selected' : ''
+                    ),
+                    array(
+                        'value'     =>  'bad',
+                        'show'      =>  'Bad',
+                        'selected'  => (isset($shipment['attributes']["condition_pt_samples"]) && $shipment['attributes']["condition_pt_samples"] == 'bad') ? 'selected' : ''
+                    ),
+                    array(
+                        'value'     =>  'not-sure',
+                        'show'      =>  'Not Sure',
+                        'selected'  => (isset($shipment['attributes']["condition_pt_samples"]) && $shipment['attributes']["condition_pt_samples"] == 'not-sure') ? 'selected' : ''
+                    )
+                );
 
-                    $section2['refridgeratorSelect'] = array(
-                        array(
-                            'value'     =>  'available',
-                            'show'      =>  'Available',
-                            'selected'  => (isset($shipment['attributes']["refridgerator"]) && $shipment['attributes']["refridgerator"] == 'available') ? 'selected' : ''
-                        ),
-                        array(
-                            'value'     =>  'not-available',
-                            'show'      =>  'Not Available',
-                            'selected'  => (isset($shipment['attributes']["refridgerator"]) && $shipment['attributes']["refridgerator"] == 'not-available') ? 'selected' : ''
-                        )
-                    );
+                $section2['refridgeratorSelect'] = array(
+                    array(
+                        'value'     =>  'available',
+                        'show'      =>  'Available',
+                        'selected'  => (isset($shipment['attributes']["refridgerator"]) && $shipment['attributes']["refridgerator"] == 'available') ? 'selected' : ''
+                    ),
+                    array(
+                        'value'     =>  'not-available',
+                        'show'      =>  'Not Available',
+                        'selected'  => (isset($shipment['attributes']["refridgerator"]) && $shipment['attributes']["refridgerator"] == 'not-available') ? 'selected' : ''
+                    )
+                );
 
-                    $section2['stopWatchSelect'] = array(
-                        array(
-                            'value'     =>  'available',
-                            'show'      =>  'Available',
-                            'selected'  => (isset($shipment['attributes']["stop_watch"]) && $shipment['attributes']["stop_watch"] == 'available') ? 'selected' : ''
-                        ),
-                        array(
-                            'value'     =>  'not-available',
-                            'show'      =>  'Not Available',
-                            'selected'  => (isset($shipment['attributes']["stop_watch"]) && $shipment['attributes']["stop_watch"] == 'not-available') ? 'selected' : ''
-                        )
-                    );
-                }
+                $section2['stopWatchSelect'] = array(
+                    array(
+                        'value'     =>  'available',
+                        'show'      =>  'Available',
+                        'selected'  => (isset($shipment['attributes']["stop_watch"]) && $shipment['attributes']["stop_watch"] == 'available') ? 'selected' : ''
+                    ),
+                    array(
+                        'value'     =>  'not-available',
+                        'show'      =>  'Not Available',
+                        'selected'  => (isset($shipment['attributes']["stop_watch"]) && $shipment['attributes']["stop_watch"] == 'not-available') ? 'selected' : ''
+                    )
+                );
+                // }
                 if ((isset($shipment['shipment_attributes']["sampleType"]) && $shipment['shipment_attributes']["sampleType"] != 'serum' && $shipment['shipment_attributes']["sampleType"] != 'plasma')) {
                     $section2['sampleRehydrationDate'] = (isset($shipment['attributes']["sample_rehydration_date"]) && $shipment['attributes']["sample_rehydration_date"] != '' && $shipment['attributes']["sample_rehydration_date"] != '0000:00:00') ? date('d-M-Y', strtotime($shipment['attributes']["sample_rehydration_date"])) : '';
                 }
