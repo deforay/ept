c78235dcf6aead74f88fe23f0a7d9b9d05363a4a
Amit Dugar <amit@deforay.com>
2023-03-30 18:34:23 +0530

    Minor changes on TB reports

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -7,28 +7,30 @@ class IndexController extends Zend_Controller_Action
     {
         /* Initialize action controller here */
     }
-    
-    public function preDispatch(){
+
+    public function preDispatch()
+    {
         $authNameSpace = new Zend_Session_Namespace('datamanagers');
-        if(isset($authNameSpace->ForcePasswordReset) && ($authNameSpace->ForcePasswordReset == 1 || $authNameSpace->ForcePasswordReset == '1')){
+        if (isset($authNameSpace->ForcePasswordReset) && ($authNameSpace->ForcePasswordReset == 1 || $authNameSpace->ForcePasswordReset == '1')) {
             $this->redirect("/participant/password");
         }
     }
 
-    public function indexAction(){
+    public function indexAction()
+    {
         $authNameSpace = new Zend_Session_Namespace('datamanagers');
         $this->_helper->layout()->activeMenu = 'home';
-        $commonServices = new Application_Service_Common(); 
+        $commonServices = new Application_Service_Common();
         $publicationService = new Application_Service_Publication();
         $partnerService = new Application_Service_Partner();
-        if(!isset($authNameSpace->dm_id)){
+        $scheme = new Application_Service_Schemes();
+        
+        if (!isset($authNameSpace->dm_id)) {
             $this->_helper->layout()->setLayout('home');
         }
         $this->view->banner = $commonServices->getHomeBanner();
         $this->view->publications = $publicationService->getAllActivePublications();
         $this->view->partners = $partnerService->getAllActivePartners();
+        $this->view->schemes = $scheme->getAllSchemes();
     }
-
-
 }
-
