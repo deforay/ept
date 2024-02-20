baae3be79fa5c061e15879cdc6ab609d6395db16
Thanaseelan <thanaseelan@deforay.com>
2024-02-20 16:29:17 +0530

    Few changes done in feed back form

--- application/models/DbTable/FeedBackTable.php
+++ application/models/DbTable/FeedBackTable.php
@@ -6,13 +6,20 @@ class Application_Model_DbTable_FeedBackTable extends Zend_Db_Table_Abstract
     protected $_name = 'r_participant_feedback_form';
     public function fetchFeedBackQuestions($sid)
     {
-        return $this->fetchAll(array("question_status = 'active'", "shipment_id = " . $sid))->toArray();
+        $db = Zend_Db_Table_Abstract::getDefaultAdapter();
+        $sql = $db->select()->from(array('rpff' => 'r_participant_feedback_form'), array('*'))
+        ->join(array('sl' => 'scheme_list'), 'rpff.scheme_type=sl.scheme_id', array('scheme_name'))
+        ->join(array('s' => 'shipment'), 'rpff.shipment_id=s.shipment_id', array('shipment_code'))
+        ->where("rpff.shipment_id =?", $sid);
+        return $db->fetchAll($sql);
     }
 
     function fetchFeedBackAnswers($sid, $pid, $mid, $type = "options"){
         $db = Zend_Db_Table_Abstract::getDefaultAdapter();
         $sql = $db->select()->from(array('pfa' => 'participant_feedback_answer'), array('*'))
         ->join(array('rpff' => 'r_participant_feedback_form'), 'pfa.question_id=rpff.question_id', array('question_text'))
+        ->join(array('sl' => 'scheme_list'), 'rpff.scheme_type=sl.scheme_id', array('scheme_name'))
+        ->join(array('s' => 'shipment'), 'pfa.shipment_id=s.shipment_id', array('shipment_code'))
         ->where("pfa.shipment_id =?", $sid)
         ->where("pfa.participant_id =?", $pid)
         ->where("pfa.map_id =?", $mid);
