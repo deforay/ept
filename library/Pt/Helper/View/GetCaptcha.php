c21c30132bb9314dcc5572435aabfbac0cfffd18
Amit Dugar <amit@deforay.com>
2024-05-08 18:18:25 +0530

    Minor change in Captcha generation

--- library/Pt/Helper/View/GetCaptcha.php
+++ library/Pt/Helper/View/GetCaptcha.php
@@ -1,5 +1,6 @@
 <?php
 
+use Gregwar\Captcha\PhraseBuilder;
 use Gregwar\Captcha\CaptchaBuilder;
 
 class Pt_Helper_View_GetCaptcha extends Zend_View_Helper_Abstract
@@ -7,14 +8,13 @@ class Pt_Helper_View_GetCaptcha extends Zend_View_Helper_Abstract
 
     public function getCaptcha()
     {
+        $phrase = null;
         //if it is development environment, then let us keep it simple
         if (APPLICATION_ENV == "development") {
             $phrase = "zaq";
-        } else {
-            $common = new Application_Service_Common();
-            $phrase = $common->generateRandomString(4);
         }
-        $builder = new CaptchaBuilder($phrase);
+        $phraseBuilder = new PhraseBuilder(4, '0123456789');
+        $builder = new CaptchaBuilder($phrase, $phraseBuilder);
         $builder->setDistortion(false);
         $builder->build(200, 100);
 
