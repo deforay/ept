2904f3d0e94b96e9047e6eaa3f354ed9ee14b310
Thanaseelan <thanaseelan@deforay.com>
2024-05-17 18:19:27 +0530

    Few changes done

--- database/migrations/7.2.2.sql
+++ database/migrations/7.2.2.sql
@@ -2,3 +2,5 @@
 ALTER TABLE `r_possibleresult` ADD `sub_scheme` VARCHAR(256) NULL DEFAULT NULL AFTER `scheme_sub_group`;
 -- Thana 17-May-2024
 ALTER TABLE `reference_result_generic_test` CHANGE `sample_score` `sample_score` DECIMAL NOT NULL DEFAULT '1';
+UPDATE reference_result_generic_test AS rrg SET reference_result = (SELECT result_code FROM r_possibleresult AS rp WHERE rp.id=rrg.reference_result);
+UPDATE response_result_generic_test AS rrg SET result = (SELECT result_code FROM r_possibleresult AS rp WHERE rp.id=rrg.result), reported_result = (SELECT result_code FROM r_possibleresult AS rp WHERE rp.id=rrg.reported_result);
