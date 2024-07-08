0e0d0ff846cafdfe35db4a51cb483203b2b69caa
Thanaseelan <thanaseelan@deforay.com>
2024-07-08 17:03:47 +0530

    In PDF VL dynamic report issue fixed

--- application/modules/admin/controllers/VlSettingsController.php
+++ application/modules/admin/controllers/VlSettingsController.php
@@ -24,31 +24,39 @@ class Admin_VlSettingsController extends Zend_Controller_Action
     }
 
     public function indexAction()
-    {
-        /** @var Zend_Controller_Request_Http $request */
-        $request = $this->getRequest();
-        $file = APPLICATION_PATH . DIRECTORY_SEPARATOR . "configs" . DIRECTORY_SEPARATOR . "config.ini";
-        if ($request->isPost()) {
-            $config = new Zend_Config_Ini($file, null, array('allowModifications' => true));
-            $sec = APPLICATION_ENV;
-
-            if (!isset($config->$sec->evaluation)) {
-                $config->$sec->evaluation = [];
+    {   
+        try{
+            /** @var Zend_Controller_Request_Http $request */
+            $request = $this->getRequest();
+            $file = APPLICATION_PATH . DIRECTORY_SEPARATOR . "configs" . DIRECTORY_SEPARATOR . "config.ini";
+            if ($request->isPost()) {
+                $config = new Zend_Config_Ini($file, null, array('allowModifications' => true));
+                $sec = APPLICATION_ENV;
+                
+                if (!isset($config->$sec->evaluation)) {
+                    $config->$sec->evaluation = [];
+                }
+                
+                if (!isset($config->$sec->evaluation->vl)) {
+                    $config->$sec->evaluation->vl = [];
+                }
+                
+                $config->$sec->evaluation->vl->passPercentage = $request->getPost('vlPassPercentage') ?? 95;
+                $config->$sec->evaluation->vl->documentationScore = $request->getPost('vlDocumentationScore') ?? 10;
+                $config->$sec->evaluation->vl->contentForIndividualVlReports = str_replace('"', "'", $request->getPost('contentForIndividualVlReports'));
+                
+                // Zend_Debug::dump($request->getPost());die;
+                $writer = new Zend_Config_Writer_Ini();
+                $writer->write($file, $config);
+                $this->view->config = new Zend_Config_Ini($file, APPLICATION_ENV);
+                $auditDb = new Application_Model_DbTable_AuditLog();
+                $auditDb->addNewAuditLog("Updated VL Settings", "config");
             }
+        } catch (Exception $exc) {
 
-            if (!isset($config->$sec->evaluation->vl)) {
-                $config->$sec->evaluation->vl = [];
-            }
-
-            $config->$sec->evaluation->vl->passPercentage = $request->getPost('vlPassPercentage') ?? 95;
-            $config->$sec->evaluation->vl->documentationScore = $request->getPost('vlDocumentationScore') ?? 95;
-            $config->$sec->evaluation->vl->contentForIndividualVlReports = $request->getPost('contentForIndividualVlReports') ?? 95;
-
-            $writer = new Zend_Config_Writer_Ini();
-            $writer->write($file, $config);
-            $this->view->config = new Zend_Config_Ini($file, APPLICATION_ENV);
-            $auditDb = new Application_Model_DbTable_AuditLog();
-            $auditDb->addNewAuditLog("Updated VL Settings", "config");
+            error_log("VL-SETTINGS-" . $exc->getMessage());
+            error_log($exc->getTraceAsString());
+            return "";
         }
 
 
