71a256b593da06cfe95439dda68a7c5e98c1df01
Sakthi Kala <sakthi@deforay.com>
2024-12-31 16:02:03 +0530

    implemented index scrren for admin to view participant mesaes

--- application/models/DbTable/ParticipantMessages.php
+++ application/models/DbTable/ParticipantMessages.php
@@ -51,4 +51,177 @@ class Application_Model_DbTable_ParticipantMessages extends Zend_Db_Table_Abstra
             return $response;
         }
     }
+
+    public function getParticipantMessage($parameters)
+    {
+
+        /* Array of database columns which should be read and sent back to DataTables. Use a space where
+         * you want to insert a non-database field (for example a counter or static image)
+         */
+
+        $aColumns = array('participant_id', 'subject', 'attached_file', 'message', 'status', "DATE_FORMAT(created_at,'%d-%b-%Y')");
+        $orderColumns = array('participant_id', 'subject', 'attached_file', 'message', 'status', "DATE_FORMAT(created_at,'%d-%b-%Y')");
+
+        /* Indexed column (used for fast and accurate table cardinality) */
+        $sIndexColumn = $this->_primary;
+
+
+        /*
+         * Paging
+         */
+        $sLimit = "";
+        if (isset($parameters['iDisplayStart']) && $parameters['iDisplayLength'] != '-1') {
+            $sOffset = $parameters['iDisplayStart'];
+            $sLimit = $parameters['iDisplayLength'];
+        }
+
+        /*
+         * Ordering
+         */
+        $sOrder = "";
+        if (isset($parameters['iSortCol_0'])) {
+            $sOrder = "";
+            for ($i = 0; $i < intval($parameters['iSortingCols']); $i++) {
+                if ($parameters['bSortable_' . intval($parameters['iSortCol_' . $i])] == "true") {
+                    $sOrder .= $orderColumns[intval($parameters['iSortCol_' . $i])] . "
+				 	" . ($parameters['sSortDir_' . $i]) . ", ";
+                }
+            }
+
+            $sOrder = substr_replace($sOrder, "", -2);
+        }
+
+        /*
+         * Filtering
+         * NOTE this does not match the built-in DataTables filtering which does it
+         * word by word on any field. It's possible to do here, but concerned about efficiency
+         * on very large tables, and MySQL's regex functionality is very limited
+         */
+        $sWhere = "";
+        if (isset($parameters['sSearch']) && $parameters['sSearch'] != "") {
+            $searchArray = explode(" ", $parameters['sSearch']);
+            $sWhereSub = "";
+            foreach ($searchArray as $search) {
+                if ($sWhereSub == "") {
+                    $sWhereSub .= "(";
+                } else {
+                    $sWhereSub .= " AND (";
+                }
+                $colSize = count($aColumns);
+
+                for ($i = 0; $i < $colSize; $i++) {
+                    if ($aColumns[$i] == "" || $aColumns[$i] == null) {
+                        continue;
+                    }
+                    if ($i < $colSize - 1) {
+                        $sWhereSub .= $aColumns[$i] . " LIKE '%" . ($search) . "%' OR ";
+                    } else {
+                        $sWhereSub .= $aColumns[$i] . " LIKE '%" . ($search) . "%' ";
+                    }
+                }
+                $sWhereSub .= ")";
+            }
+            $sWhere .= $sWhereSub;
+        }
+
+        /* Individual column filtering */
+        for ($i = 0; $i < count($aColumns); $i++) {
+            if (isset($parameters['bSearchable_' . $i]) && $parameters['bSearchable_' . $i] == "true" && $parameters['sSearch_' . $i] != '') {
+                if ($sWhere == "") {
+                    $sWhere .= $aColumns[$i] . " LIKE '%" . ($parameters['sSearch_' . $i]) . "%' ";
+                } else {
+                    $sWhere .= " AND " . $aColumns[$i] . " LIKE '%" . ($parameters['sSearch_' . $i]) . "%' ";
+                }
+            }
+        }
+
+
+        /*
+         * SQL queries
+         * Get data to display
+         */
+
+        $sQuery = $this->getAdapter()->select()
+            ->from(array('pm' => $this->_name)) // Main table
+            ->join(
+                array('dm' => 'data_manager'), // Alias for the data_manager table
+                'pm.id = dm.dm_id',          // ON condition for the LEFT JOIN
+                array('first_name', 'last_name')            // Columns to select from the joined table
+            );
+        // ->joinLeft(array('sl' => 'scheme_list'), 's.scheme_type=sl.scheme_id', array('scheme_name'))
+
+        if (isset($sWhere) && $sWhere != "") {
+            $sQuery = $sQuery->where($sWhere);
+        }
+
+        if (!empty($sOrder)) {
+            $sQuery = $sQuery->order($sOrder);
+        }
+
+        if (isset($sLimit) && isset($sOffset)) {
+            $sQuery = $sQuery->limit($sLimit, $sOffset);
+        }
+
+        //die($sQuery);
+
+        $rResult = $this->getAdapter()->fetchAll($sQuery);
+
+
+        /* Data set length after filtering */
+        $sQuery = $sQuery->reset(Zend_Db_Select::LIMIT_COUNT);
+        $sQuery = $sQuery->reset(Zend_Db_Select::LIMIT_OFFSET);
+        $aResultFilterTotal = $this->getAdapter()->fetchAll($sQuery);
+        $iFilteredTotal = count($aResultFilterTotal);
+
+        /* Total data set length */
+        $sQuery = $this->getAdapter()->select()->from($this->_name, new Zend_Db_Expr("COUNT('" . $sIndexColumn . "')"));
+        $aResultTotal = $this->getAdapter()->fetchCol($sQuery);
+        $iTotal = $aResultTotal[0];
+
+        /*
+         * Output
+         */
+        $output = array(
+            "sEcho" => intval($parameters['sEcho']),
+            "iTotalRecords" => $iTotal,
+            "iTotalDisplayRecords" => $iFilteredTotal,
+            "aaData" => array()
+        );
+
+        foreach ($rResult as $aRow) {
+            // Check if first_name or last_name is missing and set default if necessary
+            $firstName = isset($aRow['first_name']) ? $aRow['first_name'] : '';
+            $lastName = isset($aRow['last_name']) ? $aRow['last_name'] : '';
+
+            // Concatenate first and last name for participant name
+            $participantName = ucwords($firstName . ' ' . $lastName);
+
+            $row = [];
+            $row[] = $participantName;
+            $row[] = ucwords($aRow['subject']);
+            $row[] = ucwords($aRow['message']);
+            $row[] = Pt_Commons_General::humanReadableDateFormat($aRow['created_at']);
+            $row[] = '<a class="btn btn-primary btn-xs" href="/admin/participant-messages/view/d8s5_8d/' . base64_encode($aRow['id']) . '"><span><i class="fa fa-eye"></i> view</span></a>';
+            $output['aaData'][] = $row;
+            // print_r($output); die;
+        }
+
+        // Add this code block for debugging and safe JSON encoding
+        header('Content-Type: application/json'); // Set the response type to JSON
+
+        try {
+            $json = json_encode($output); // Encode the output array to JSON
+            if ($json === false) {
+                // JSON encoding error
+                echo json_last_error_msg(); // Print error details
+                exit;
+            }
+            echo $json; // Send the JSON response to the client
+        } catch (Exception $e) {
+            // Handle unexpected exceptions
+            echo json_encode(['error' => $e->getMessage()]);
+        }
+        exit; // Ensure no additional output is sent
+
+    }
 }
