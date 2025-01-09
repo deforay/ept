64148f0b1a99a176c021db2b2bd0a4352df572bd
Amit Dugar <amit@deforay.com>
2025-01-09 20:38:27 +0530

    Testkit issue

--- application/models/Dts.php
+++ application/models/Dts.php
@@ -1442,7 +1442,7 @@ class Application_Model_Dts
 					'attributes'
 				]
 			)
-			->joinLeft(['stm' => 'scheme_testkit_map'], 't.TESTKITNAMEID = stm.testkit_id', ['scheme_type', 'testkit_1', 'testkit_2', 'testkit_3'])
+			->joinLeft(['stm' => 'scheme_testkit_map'], 't.TestKitName_ID = stm.testkit_id', ['scheme_type', 'testkit_1', 'testkit_2', 'testkit_3'])
 			->order("TESTKITNAME ASC");
 		if (isset($stage) && !empty($stage) && !in_array($stage, ['testkit_1', 'testkit_2', 'testkit_3'])) {
 			if ($stage == 'custom-tests')
