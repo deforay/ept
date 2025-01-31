84f2dcc8b63e0208899b755fd9fc0e9996c06dfe
Thanaseelan <thanaseelan@deforay.com>
2025-01-31 19:17:08 +0530

    Duplicate sample label fixed

--- application/services/Evaluation.php
+++ application/services/Evaluation.php
@@ -1566,7 +1566,8 @@ class Application_Service_Evaluation
 					// ->where("refeid.control = 0")
 					->where(new Zend_Db_Expr("IFNULL(spm.is_excluded, 'no') = 'no'  OR spm.is_excluded = '' "))
 					->where("reseid.shipment_map_id = ?", $res['map_id'])
-					->order(array('refeid.sample_id'));
+					->order(array('refeid.sample_id'))
+					->group(array('refeid.sample_id'));
 				$result = $db->fetchAll($sQuery);
 				$response = [];
 				foreach ($result as $key => $row) {
