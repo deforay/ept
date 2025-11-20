32e1b433d7b7bfcc9ce5944ecb8db3f0fbabecda
Thanaseelan <thanaseelan@deforay.com>
2025-11-20 19:54:46 +0530

    Updated delete option for Responded tap on enrolment page

--- application/models/DbTable/Participants.php
+++ application/models/DbTable/Participants.php
@@ -1095,7 +1095,7 @@ class Application_Model_DbTable_Participants extends Zend_Db_Table_Abstract
             $row[] = $aRow['phone'];
             $row[] = $aRow['affiliation'];
             $row[] = $aRow['email'];
-
+            $row[] = '<a href="javascript:void(0);" onclick="removeParticipants(\'' . base64_encode($aRow['map_id']) . '\',\'' . base64_encode($aRow['shipment_id']) . '\')" class="btn btn-primary btn-xs"><i class="icon-remove"></i> Delete</a>';
             $output['aaData'][] = $row;
         }
 
