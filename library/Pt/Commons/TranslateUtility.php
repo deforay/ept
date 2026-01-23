68483121dcc0b0a58b1982e8a97124585a31c813
Amit Dugar <amit@deforay.com>
2026-01-23 22:03:01 +0530

    Expanded coverage of Translations

--- library/Pt/Commons/TranslateUtility.php
+++ library/Pt/Commons/TranslateUtility.php
@@ -23,4 +23,32 @@ final class Pt_Commons_TranslateUtility
             return $text;
         }
     }
+
+    /**
+     * Translate a string and escape it for use in HTML content/attributes.
+     *
+     * @param string|null $text The text to translate
+     * @return string The translated string safe for HTML
+     */
+    public static function htmlTranslate(?string $text): string
+    {
+        return htmlspecialchars(self::safeTranslate($text), ENT_QUOTES, 'UTF-8');
+    }
+
+    /**
+     * Translate a string and escape it for use in JavaScript.
+     *
+     * @param string|null $text The text to translate
+     * @return string The translated string safe for JavaScript
+     */
+    public static function jsTranslate(?string $text): string
+    {
+        $translated = self::safeTranslate($text);
+        $encoded = json_encode(
+            $translated,
+            JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
+        );
+        // Remove surrounding quotes added by json_encode
+        return substr($encoded, 1, -1);
+    }
 }
