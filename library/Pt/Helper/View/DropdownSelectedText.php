6f4c58b5f62042a0ea408acffdd767da23a00383
Amit Dugar <amit@deforay.com>
2026-04-07 22:06:21 +0530

    ViewHelper Translation update

--- library/Pt/Helper/View/DropdownSelectedText.php
+++ library/Pt/Helper/View/DropdownSelectedText.php
@@ -1,11 +1,19 @@
 <?php
-class  Pt_Helper_View_DropdownSelectedText extends Zend_View_Helper_Abstract {
-	
-	public function dropdownSelectedText( $allRecord, $selection= ""){
-		foreach ($allRecord as $key=> $value){
-			if($selection == $key) { 
-				echo $value;
-			}
-		}
-	}
+class Pt_Helper_View_DropdownSelectedText extends Zend_View_Helper_Abstract
+{
+    public function dropdownSelectedText($allRecord, $selection = "")
+    {
+        $translator = $this->view->translate ?? Zend_Registry::get('translate');
+
+        foreach ($allRecord as $key => $value) {
+            if ($selection == $key) {
+                // Keep selected-text rendering aligned with dropdownSelection translations.
+                $translatedValue = ($value === null || $value === '')
+                    ? ''
+                    : htmlspecialchars($translator->_((string) $value), ENT_QUOTES, 'UTF-8');
+
+                echo $translatedValue;
+            }
+        }
+    }
 }
