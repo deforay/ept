d41eed36d7bdc8f1b08576a57de70d247b71879a
Amit Dugar <amit@deforay.com>
2026-05-08 22:42:11 +0530

    Split 7.4.2 ALTERs into separate statements

--- database/migrations/7.4.2.sql
+++ database/migrations/7.4.2.sql
@@ -5,6 +5,10 @@
 UPDATE `system_config` SET `value` = '7.4.2' WHERE `config` = 'app_version';
 
 ALTER TABLE `data_manager`
-    ADD COLUMN `password_reset_token` CHAR(64) NULL DEFAULT NULL AFTER `password`,
-    ADD COLUMN `password_reset_expires_at` DATETIME NULL DEFAULT NULL AFTER `password_reset_token`,
+    ADD COLUMN `password_reset_token` CHAR(64) NULL DEFAULT NULL AFTER `password`;
+
+ALTER TABLE `data_manager`
+    ADD COLUMN `password_reset_expires_at` DATETIME NULL DEFAULT NULL AFTER `password_reset_token`;
+
+ALTER TABLE `data_manager`
     ADD UNIQUE KEY `uniq_data_manager_pw_reset_token` (`password_reset_token`);
