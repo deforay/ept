1c9682d40e38d82615ce0e29006063aa3f1401be
Amit Dugar <amit@deforay.com>
2026-05-09 00:24:34 +0530

    Remove dead PTCC-side data-managers add/edit views and controller actions

--- application/controllers/DataManagersController.php
+++ application/controllers/DataManagersController.php
@@ -30,53 +30,6 @@ class DataManagersController extends Zend_Controller_Action
         }
     }
 
-    public function addAction()
-    {
-        $userService = new Application_Service_DataManagers();
-        $commonService = new Application_Service_Common();
-        $participantService = new Application_Service_Participants();
-        /** @var Zend_Controller_Request_Http $request */
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            $params = $request->getPost();
-            $userService->addUser($params);
-            $this->redirect('/data-managers');
-        } else {
-            $this->view->participants = $participantService->getAllActiveParticipants();
-        }
-        if ($this->hasParam('contact')) {
-            $contact = new Application_Model_DbTable_ContactUs();
-            $this->view->contact = $contact->getContact($this->_getParam('contact'));
-        }
-        $globalConfigDb = new Application_Model_DbTable_GlobalConfig();
-        $this->view->passLength = $globalConfigDb->getValue('participant_login_password_length');
-        $this->view->countriesList = $commonService->getcountriesList();
-    }
-
-    public function editAction()
-    {
-        $participantService = new Application_Service_Participants();
-        $userService = new Application_Service_DataManagers();
-        $commonService = new Application_Service_Common();
-        /** @var Zend_Controller_Request_Http $request */
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            $params = $request->getPost();
-            $userService->updateUser($params);
-            $this->redirect('/data-managers');
-        } else {
-            if ($this->hasParam('id')) {
-                $userId = (int) $this->_getParam('id');
-                $this->view->rsUser = $userService->getUserInfoBySystemId($userId);
-                $this->view->participants = $participantService->getAllActiveParticipants();
-                $this->view->participantList = $participantService->getActiveParticipantDetails($userId);
-                $this->view->countriesList = $commonService->getcountriesList();
-                $globalConfigDb = new Application_Model_DbTable_GlobalConfig();
-                $this->view->passLength = $globalConfigDb->getValue('participant_login_password_length');
-            }
-        }
-    }
-
     public function getParticipantsNamesAction()
     {
         $this->_helper->layout()->disableLayout();
