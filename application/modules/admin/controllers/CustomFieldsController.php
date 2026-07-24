f99178750c83f1552c6a8ae10ed5f9d7427f3ad9
Amit Dugar <amit@deforay.com>
2026-07-25 01:32:34 +0530

    Fold shipment custom fields page into global config

--- application/modules/admin/controllers/CustomFieldsController.php
+++ application/modules/admin/controllers/CustomFieldsController.php
@@ -1,48 +1,10 @@
 <?php
 
+// Folded into ePT Global Settings; kept only so old bookmarks still land somewhere.
 class Admin_CustomFieldsController extends Zend_Controller_Action
 {
-    public function init()
-    {
-        /** @var Zend_Controller_Request_Http $request */
-        $request = $this->getRequest();
-        $adminSession = new Zend_Session_Namespace('administrators');
-        $privileges = explode(',', $adminSession->privileges);
-        if (!in_array('config-ept', $privileges)) {
-            if ($request->isXmlHttpRequest()) {
-                // init() returning does not abort ZF1 dispatch; halt so the
-                // action never runs for unauthorized XHR callers.
-                $this->getResponse()->setHttpResponseCode(403)->sendResponse();
-                exit;
-            }
-            $this->redirect('/admin');
-            return;
-        }
-        $this->_helper->layout()->pageName = 'configMenu';
-    }
-
     public function indexAction()
     {
-        /** @var Zend_Controller_Request_Http $request */
-        $request = $this->getRequest();
-        $globalConfigDb = new Application_Model_DbTable_GlobalConfig();
-        if ($request->isPost()) {
-            $customField1 = $this->_getParam('customField1', '');
-            $customField2 = $this->_getParam('customField2', '');
-            $haveCustom = $this->_getParam('haveCustom', 'no');
-            $db = Zend_Db_Table_Abstract::getDefaultAdapter();
-            $customFieldInConfig = $globalConfigDb->getValue('custom_field_1');
-
-            $configUpdate = [
-                'custom_field_1' => ($haveCustom == 'yes') ? $customField1 : '',
-                'custom_field_2' => ($haveCustom == 'yes') ? $customField2 : '',
-                'custom_field_needed' => $haveCustom,
-            ];
-            $globalConfigDb->updateConfigDetails($configUpdate);
-        }
-
-        $this->view->customField1 = $globalConfigDb->getValue('custom_field_1');
-        $this->view->customField2 = $globalConfigDb->getValue('custom_field_2');
-        $this->view->haveCustom = $globalConfigDb->getValue('custom_field_needed');
+        $this->redirect('/admin/global-config#customFieldsConfig');
     }
 }
